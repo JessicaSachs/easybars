d907d487345ced9451337ed2c7e7f872d8a057f4
corwin <corwin@redhat.com>
2018-04-09 02:48:34 -0400

    corwin: re-enabled tests   test/specs/edgecase.spec.js:     Re-enabled empty string test.
      test/specs/sections/if.spec.js:
        Re-enabled all recently disabled tests.

--- test/specs/edgecase.spec.js
+++ test/specs/edgecase.spec.js
@@ -1,17 +1,17 @@
-// var Easybars = require('../../index');
-//
-// describe('protect against edge cases', function () {
-//
-//     describe('when the template string contains nothing to interpolate', function (expect) {
-//     	var output = Easybars('foo bar baz', { foo : true });
-// 	    expect(output).toBe('foo bar baz');
-//     });
-//     xdescribe('when the template string contains a "length" key', function (expect) {
-//         var str = '{{length}}';
-//         var data = { length: 'hello!' };
-//         var render = new Easybars().compile(str);
-//         var output = render(data);
-//         expect(output).toBe('hello!');
-//     });
-//
-// });
+var Easybars = require('../../index');
+
+describe('protect against edge cases', function () {
+
+    describe('when the template string contains nothing to interpolate', function (expect) {
+    	var output = Easybars('foo bar baz', { foo : true });
+	    expect(output).toBe('foo bar baz');
+    });
+    xdescribe('when the template string contains a "length" key', function (expect) {
+        var str = '{{length}}';
+        var data = { length: 'hello!' };
+        var render = new Easybars().compile(str);
+        var output = render(data);
+        expect(output).toBe('hello!');
+    });
+
+});
